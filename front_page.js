efd8250c4c6b54c6fec068331a933d87e4ab7d03
wahlforss <alfred@wahlforss.com>
2017-04-06 14:08:47 +0200

    Added everything

---
+++ front_page.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	randomizeBackground()
+	randomizeData()
+	$('#wrapper').css("visibility", "visible");
+	
+	
+});
+
+//picking a random picture as background
+function randomizeBackground() {
+	var trainRandom = Math.floor(Math.random() * 5)
+	var trainImage = 'url(http://localhost:3000/green/train/train' + trainRandom + '.jpg)'
+	$('#train').css("background-image", trainImage)
+	var airplaneRandom = Math.floor(Math.random() * 5)
+	var airplaneImage = 'url(http://localhost:3000/green/airplane/airplane' + trainRandom + '.jpg)'
+	$('#plane').css("background-image", airplaneImage)
+
+}
+
+function randomizeData() {
+	$.getJSON("train.json", function(json) {
+		var randomSelect = Math.floor(Math.random() * Object.keys(json).length);
+    	var trainObject = json[randomSelect];
+    	$("#trainDest").text(trainObject.Destination)
+    	$("#trainCarbon").text(trainObject.Carbon)
+    	$("#trainCost").text(trainObject.Cost)
+    	console.log(trainObject.Destination);
+});
+	$.getJSON("airplane.json", function(json) {
+		var randomSelectA = Math.floor(Math.random() * Object.keys(json).length);
+		var planeObject = json[randomSelectA];
+    	$("#planeDest").text(planeObject.Destination)
+    	$("#planeCarbon").text(planeObject.Carbon)
+    	$("#planeCost").text(planeObject.Cost)
+    	console.log(planeObject);
+	});
+
+}
---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express')
+var app = express();
+var http = require('http').Server(app);
+
+app.set('port', (process.env.PORT || 3000));
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/front_page.html');
+});
+app.use(express.static(__dirname + '/images'));
+
+
+app.get('/front_page.css', function(req, res){
+	res.sendFile(__dirname + '/front_page.css');
+});
+app.get('/train.json', function(req, res){
+	res.sendFile(__dirname + '/train.json');
+});
+
+app.get('/airplane.json', function(req, res){
+	res.sendFile(__dirname + '/airplane.json');
+});
+
+app.get('/front_page.js', function(req, res){
+	res.sendFile(__dirname + '/front_page.js')
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+//app.use(express.static('images'))
